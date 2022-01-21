f45825573b9145aff6dd1cd4557f63b1a32f6fd4
purplemrl <nikoo79m@gmail.com>
2022-01-21 16:56:46 +0330

    Update ex1.js

--- Udemi Js Course/ex1.js
+++ Udemi Js Course/ex1.js
@@ -1,3 +1,4 @@
+//test1
 //mark mass and height
 const markMass=78;
 const markHeight=1.69;
@@ -7,8 +8,22 @@ const johnMass=92;
 const johnHeight=1.95;
 
 //BMI test
-let markBMI, johnBMI;
-markBMI= markMass/markHeight**2;
-johnBMI= johnMass/johnHeight**2;
+const markBMI= markMass/markHeight**2;
+const johnBMI= johnMass/johnHeight**2;
+const markHigherBMI= markBMI > johnBMI;
+console.log(markBMI , johnBMI , markHigherBMI);
 
-console.log(markBMI , johnBMI);
+//test2
+//mark mass and height
+const markMass2=95;
+const markHeight2=1.88;
+
+//john mass and height
+const johnMass2=85;
+const johnHeight2=1.76;
+
+//BMI test
+const markBMI2=markMass2/markHeight2**2;
+const johnBMI2=johnMass2/johnHeight2**2;
+const markHigherBMI2= markBMI2 > johnBMI2;
+console.log(markBMI2 , johnBMI2 , markHigherBMI2);
