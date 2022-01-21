acc9b90649234aa7172cb94696b715103201680a
purplemrl <nikoo79m@gmail.com>
2022-01-21 16:47:36 +0330

    first

---
+++ Udemi Js Course/ex1.js
@@ -0,0 +1,14 @@
+//mark mass and height
+const markMass=78;
+const markHeight=1.69;
+
+//john mass and height
+const johnMass=92;
+const johnHeight=1.95;
+
+//BMI test
+let markBMI, johnBMI;
+markBMI= markMass/markHeight**2;
+johnBMI= johnMass/johnHeight**2;
+
+console.log(markBMI , johnBMI);
